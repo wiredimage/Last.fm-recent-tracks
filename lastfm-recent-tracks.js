1c7dfd5a5b2366210ad6dbc6e69256b8f5011621
Ben Armstrong <ben@wiredimage.co.uk>
2016-01-30 01:56:30 +0000

    Minor updates to layout

--- lastfm-recent-tracks.js
+++ lastfm-recent-tracks.js
@@ -1,16 +1,9 @@
-function recentLFM(user,limit,imgsize,container) {
+function recentLFM(user,limit,imgsize,divider,container) {
 	// Defaults
 	var counter = 0;
-	var list = '<ul class=\"lfm-recent\"></ul>';
-	var divider = ' &ndash; ';
+	var listwrap = '<ul class=\"lfm-recent\"></ul>';
 	var apikey = '048785ed2aa0722643cb5c81cc39baa2'; // Generate your own API Key for use
-	var imgsize = 1;
 	var html = '';
-	var limit = 3;
-	
-	var user = $("div").data('lfm-user');
-	var limit = $("div").data('lfm-limit');
-	var imgsize = $("div").data('lfm-imgsize');
 	
 	$.getJSON("http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user="+user+"&api_key="+apikey+"&limit="+limit+"&format=json&callback=?", function(data) {
 		
@@ -19,19 +12,24 @@ function recentLFM(user,limit,imgsize,container) {
             var artistlink = item.url.split('_/');
             
             // Loop through data
-            if(i < (limit)) {
-                html += '<li class="lfm-track"><a href="'+item.url+'" class="lfm-artork" target="_blank"><img src="'+item.image[imgsize]['#text']+'" alt="Album cover for '+item.album['#text']+'" /></a><span class="lfm-trackname"><a href="'+item.url+'">'+item.name+'</a></span>'+divider+'<span class="lfm-artist"><a href="'+artistlink[0]+'" target="_blank">'+item.artist['#text']+'</a></span></li>';
-			}
+            if(i < limit) {
+                html += '<li class="lfm-track"><a href="'+item.url+'" target="_blank"><img src="'+item.image[imgsize]['#text']+'" class="lfm-artwork" alt="Album cover for '+item.album['#text']+'" /></a><span class="lfm-trackname"><a href="'+item.url+'">'+item.name+'</a></span>'+divider+'<span class="lfm-artist"><a href="'+artistlink[0]+'" target="_blank">'+item.artist['#text']+'</a></span></li>';
+				}
             i++ // add 1 to the counter variable each time the each loop runs
         }); // close each loop
         
-        $(container).append(html).wrapInner(list);
+        $(container).append(html).wrapInner(listwrap); // Append html to the container wrapped in the list
 		
 		});
 } // End recentLFM function
 
 $(document).ready(function() {
 	
-	recentLFM(user,limit,imgsize,'.lfm');
+	var user = $("div").data('lfm-user');
+	var limit = $("div").data('lfm-limit');
+	var imgsize = $("div").data('lfm-imgsize');
+	var divider = $("div").data('lfm-divider');
+	
+	recentLFM(user,limit,imgsize,divider,'.lfm');
 	
 }); // End document ready function
